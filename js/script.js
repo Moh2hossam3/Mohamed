60d3d371f6e1ac94a5495192951af7554d8222eb
Moh2hossam3 <118656006+Moh2hossam3@users.noreply.github.com>
2022-12-24 05:59:36 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById("menu");
+const action = document.getElementById("action");
+
+menu.addEventListener("click", () => {
+    hundleenu();
+});
+
+
+function hundlemenu() {
+  menu.claalist.toggle("is-active");
+  action.claalist.toggle("is-active");
+}
+
